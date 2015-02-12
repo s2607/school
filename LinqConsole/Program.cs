ca7ae2bdbf6e2a7125bdf526bfe4794eb08efc7a
Stephen Wiley <sww2607@email.vccs.edu>
2015-02-11 19:21:00 -0500

    works

---
+++ LinqConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConsole
+{
+    class Program
+    {
+        static void waitkey()
+        {
+            Console.WriteLine("press any key");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            String foo = "bar baz";
+            Console.WriteLine("foo.left(33) returned {0}", foo.Left(33));
+            Console.WriteLine("foo.left(3) returned {0}", foo.Left(3));
+            Console.WriteLine("foo.right(3) returned {0}", foo.Right(3));
+            Console.WriteLine("foo.right(33) returned {0}", foo.Right(33));
+          // waitkey();
+  
+        }
+    }
+}
---
+++ LinqConsole/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConsole
+{
+    //we will write an extension to the class "String"
+    //we will create a method
+    public static class StringExtension 
+    {
+        //just like golang :D
+        //normally you would not put these in your user interface project:
+        //you would create your own project just for extensions
+        //                          V --- special modifier
+        public static string Left(this string name, int length)
+        {
+            //alot of times you will return one of two things
+            // the name if the length isn't logn enough or part of the name if it isnt
+            // ternery operators explained
+            return name == null ? null : name.Length <length ? name : name.Substring(0,length);
+        }
+
+        public static string Right(this string name, int length)
+        {
+            return name == null ? null : name.Length < length ? name : name.Substring(length);
+        }
+    }
+}
